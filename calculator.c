507ef6e64ca5d237ccd1039a4d246b1e51c7c4c2
Sudhay <59689805+sudhay23@users.noreply.github.com>
2021-05-12 20:15:13 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -31,11 +31,59 @@ double add(double a, double b) {
 // Insert your code below
 
 int main() {
-    // Initialize variables
+    int choice;
+    printf(
+        "Welcome to the Cool Calculator program!\n\n"
+        "1. Addition\n"
+        "2. Subtraction\n"
+        "3. Multiplication\n"
+        "4. Division\n"
+        "5. Exponentiation\n"
+        "6. Sine\n"
+        "7. Cosine\n"
+        "8. Tangent\n"
+        "9. Floor\n"
+        "10. Ceiling\n"
+        "11. Absolute value\n\n"
+        "Enter your choice: ");
 
-    // Prompt user for input
+    scanf("%d", &choice);
 
-    // Perform appropriate function
+    switch (choice) {
+        // 1. Addition
+        case 1:
+            printf("\nEnter two numbers: \n");
+            int a, b;
+            scanf("%d%d", &a, &b);
+            int result = a + b;
+            printf("The sum is %d\n", result);
+            break;
 
-    // Terminate program after finishing tasks
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+
+        // 6. Sine
+        case 6:
+            printf("\nEnter the angle in degrees: ");
+            int angle;
+            scanf("%d", &angle);
+
+            // Convert degrees to radians
+            double radians = angle / 180.0 * 3.14;
+            double answer = sin(radians);
+
+            printf("The sine value is %f", answer);
+            break;
+
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        default:
+            printf("Invalid choice!");
+    }
+    return 0;
 }
